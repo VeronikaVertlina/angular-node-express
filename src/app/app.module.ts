3a0ac6f74e1a66f1b06006d73b1d4e53fe62ad49
ronya78@gmail.com <ronya78@gmail.com>
2022-01-23 14:27:39 +0200

    init code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputUserDataFormComponent } from './input-user-data-form/input-user-data-form.component';
+import { DisplayUserDataComponent } from './display-user-data/display-user-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputUserDataFormComponent,
+    DisplayUserDataComponent
   ],
   imports: [
     BrowserModule
